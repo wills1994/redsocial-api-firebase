8072c25c7ef6555de45ec795f852fa17d5497c4c
Wilson Pinto Dsouza <wills1994@hotmail.com>
2020-07-28 15:30:43 +0200

    socialapp-api-firebase new

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports= {
+    apiKey: "AIzaSyD4WA2BCfa1PcFqhxzBdThSBsL1JuJqY7U",
+    authDomain: "socialape-61d23.firebaseapp.com",
+    databaseURL: "https://socialape-61d23.firebaseio.com",
+    projectId: "socialape-61d23",
+    storageBucket: "socialape-61d23.appspot.com",
+    messagingSenderId: "976154811039",
+    appId: "1:976154811039:web:9339998d083d5793a2a0aa",
+    measurementId: "G-1EE5EFRYVL"
+};
